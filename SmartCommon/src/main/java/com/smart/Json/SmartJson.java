20b77519ac6e77e7f0fb36451fd995223162873c
Administrator <Administrator@mendel>
2015-09-02 09:39:58 +0800

    pad sign in

---
+++ SmartCommon/src/main/java/com/smart/Json/SmartJson.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.smart.Json;
+
+import com.google.gson.Gson;
+
+/**
+ *
+ * @author Administrator
+ */
+public class SmartJson {
+
+    public Gson Json;
+    public SmartJson() {
+        this.Json = new Gson();
+    }
+}
